43fd7ae09e11209183d89e0939ab1cf1ca7b0221
vishnukai <94909159+vishnukai@users.noreply.github.com>
2022-06-17 16:56:59 +0530

    Update views.py

--- tally/tallyapp/views.py
+++ tally/tallyapp/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
+from tallyapp.models import  groups,ledger,bank
+from django.db.models import Count
 
 # Create your views here.
 def home(request):
-    return render(request,'tallynew.html')
+    return render(request,'base.html')
+
+def chequeprinting(request):
+    return render(request,'chequeprinting.html')
+
+def chequeregister(request):
+    led=ledger.objects.all()   
+    bak=bank.objects.all().values('ledger').annotate(total=Count('ledger'))
+    print(bak)
+    
+        
+           
+
+
+    
+    
+    return render(request,'chequeregister.html',{'bak':bak})
+
+
+def montlysummary(request):
+    return render(request,'montlysummary.html')
+
+def searchbar(request):
+    group=groups.objects.get(group="Bank Accounts")
+    print(group)
+    led=ledger.objects.filter(group=group.id)
+    print(led)
+
+    return render(request,'searchbar.html',{'l':led})
+
+def searchledger(request):
+    return render(request,'searhbarledger.html')
+
+
+def chequep(request,id):
+    bak=bank.objects.filter(ledger=id)
+    return render(request,'chequeprinting.html',{'bank':bak})
+
+
+
+
+
+
 
